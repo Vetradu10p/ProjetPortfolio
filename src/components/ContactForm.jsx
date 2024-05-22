d2631d5d58007ba88ad060269cd51af0591e09ce
Vetradu10P <vetradu10p@gmail.com>
2024-05-23 00:06:35 +0200

    Portfolio fini! Prochaine étape: responsive

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+
+import '@/assets/Css/contactForm.css';
+
+
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form data:', formData);
+  };
+
+  return (
+    <form className='FormContainer' onSubmit={handleSubmit}>
+        <div className="NamemailContainer">  
+            <div className='NameContact'>
+                <label htmlFor="name"></label>
+                <input 
+                type="text" 
+                id="name" 
+                name="name" 
+                placeholder='Votre nom & prénom'
+                value={formData.name} 
+                onChange={handleChange} 
+                required 
+                />
+            </div>
+            <div className='EmailContact'>
+                <label htmlFor="email"></label>
+                <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                placeholder='Votre Email'
+                value={formData.email} 
+                onChange={handleChange} 
+                required 
+                />
+            </div>
+        </div>
+      <div>
+        <label htmlFor="message"></label>
+        <textarea 
+        className='MessageForm'
+          id="message" 
+          name="message"
+          placeholder='Écrivez votre message'
+          value={formData.message} 
+          onChange={handleChange} 
+          required 
+        ></textarea>
+      </div>
+      <button className='SubmitButton' type="submit">Envoyer le message</button>
+    </form>
+  );
+};
+
+export default ContactForm;
