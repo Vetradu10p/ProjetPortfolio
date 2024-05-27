1d520691aedd73785736111f3d5127932d1478a6
Vetradu10P <vetradu10p@gmail.com>
2024-05-27 03:39:55 +0200

    Aim

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Card from '@/components/card/Card';
 import WorksService from '@/_Service/Works.service';
-import '@/assets/Css/work.css';
+import '@/assets/Css/components/work.css';
 
 export default function Work() {
   return (
