91837953fdbca0c6a682d946c346e21c2325e138
Vetradu10P <vetradu10p@gmail.com>
2024-05-30 14:06:37 +0200

    Contactform fusion

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import ContactForm from '../contactForm/ContactForm'
+import React, { useState } from 'react'
 
 import ImgContact from "@/assets/Images/Contact.jpg"
 import Linkedin from '@/assets/Images/Linkedin.png';
 import Instagram from '@/assets/Images/Instagram.png';
 
 export default function Contact() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form data:', formData);
+  };
+
   return (
     <section className='ContactContainer'>
         <div className='ImgContainer'>
@@ -28,7 +47,48 @@ export default function Contact() {
               <img className='Instagram' src={Instagram} alt="Logo d'Instagram" />
               </a>          
             </div>
-            <ContactForm />
+
+    <form className='FormContainer' onSubmit={handleSubmit}>
+        <div className="NamemailContainer">  
+            <div className='NameContact'>
+                <label htmlFor="name"></label>
+                <input 
+                type="text" 
+                id="name" 
+                name="name" 
+                placeholder='Votre nom & prénom'
+                value={formData.name} 
+                onChange={handleChange} 
+                required 
+                />
+            </div>
+            <div className='EmailContact'>
+                <label htmlFor="email"></label>
+                <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                placeholder='Votre Email'
+                value={formData.email} 
+                onChange={handleChange} 
+                required 
+                />
+            </div>
+        </div>
+      <div className='MessageContact'>
+        <label htmlFor="message"></label>
+        <textarea 
+        className='MessageForm'
+          id="message" 
+          name="message"
+          placeholder='Écrivez votre message'
+          value={formData.message} 
+          onChange={handleChange} 
+          required 
+        ></textarea>
+      </div>
+      <button className='SubmitButton' type="submit">Envoyer le message</button>
+    </form>
         </div>
     </section>
   )
